554da6e47c0b5676c31c735dfa7924753a8accd0
LeshMesh <ivlex37rus@gmail.com>
2021-12-15 20:06:40 +0300

    Added selectors and assert.

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -1,9 +1,12 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
-import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTest {
     @Test
@@ -14,11 +17,29 @@ public class PracticeFormTest {
         $("#userEmail").setValue("user@mail.com");
         $("[for=gender-radio-1]").click();
         $("#userNumber").setValue("89991234567");
-//        $("#dateOfBirthInput").setValue("");
-//        $(".subjects-auto-complete__control").setValue("Programming");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $("[value='10']").click();
+        $(".react-datepicker__year-select").click();
+        $("[value='1992']").click();
+        $(".react-datepicker__day--002").click();
+        $("#subjectsInput").setValue("C");
+        $(Selectors.byText("Computer Science")).scrollTo().click();
         $("[for=hobbies-checkbox-1]").click();
         $("[for=hobbies-checkbox-3]").click();
         $("input#uploadPicture").uploadFile(new File("C:\\Users\\ivlex\\Downloads\\dog.jpg"));
         $("#currentAddress").setValue("Russia, Moscow");
+        $(Selectors.byText("Select State")).click();
+        $(Selectors.byText("Haryana")).click();
+        $(Selectors.byText("Select City")).click();
+        $(Selectors.byText("Karnal")).click();
+        $("#submit").click();
+
+        $(".modal-content").shouldBe(Condition.visible);
+        $(".modal-content").shouldHave(text("Alexey Belyakov"), text("user@mail.com"),
+                text("Male"), text("8999123456"), text("02 November,1992"), text("Computer Science"),
+                text("Sports, Music"), text("dog.jpg"), text("Russia, Moscow"), text("Haryana Karnal"));
+
+
     }
 }
