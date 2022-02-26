bfd99461fe08ddb4fd163f39c73b744e391e04b2
LeshMesh <ivlex37rus@gmail.com>
2022-02-26 16:05:16 +0300

    Edit TestBase

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -3,6 +3,7 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
@@ -12,6 +13,11 @@ public class TestBase {
 
         Configuration.browser = "CHROME";
         Configuration.browserSize = "1920x1080";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
